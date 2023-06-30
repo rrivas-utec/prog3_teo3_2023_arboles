f438db8fa5a690a3a087d4055b7ba5705eb3e892
rrivas <rrivas@utec.edu.pe>
2023-06-30 08:55:50 -0500

    Update Commit

--- binary_search_tree.h
+++ binary_search_tree.h
@@ -7,6 +7,7 @@
 
 #include <functional>
 #include <memory>
+#include <queue>
 using namespace std;
 
 template <
@@ -15,20 +16,90 @@ template <
         typename Compare= std::less<KeyType>
         >
 class binary_search_tree {
+    // Alias de tipos
+    struct node_t;  // declaracion adelantada (forward)
+    using item_t = pair<KeyType, ValueType>;
+    using shared_ptr_t = shared_ptr<node_t>;
+    using weak_ptr_t = weak_ptr<node_t>;
+
     struct node_t {
-        pair<KeyType, ValueType> item;
-        shared_ptr<node_t> left;
-        shared_ptr<node_t> right;
-        weak_ptr<node_t> parent;
-        node_t(pair<KeyType, ValueType> item, shared_ptr<node_t> parent):
+        item_t item;
+        shared_ptr_t left;
+        shared_ptr_t right;
+        weak_ptr_t parent;
+        node_t(item_t item, shared_ptr_t parent):
             item(item), parent(parent) {}
     };
 
-    shared_ptr<node_t> root;
+    shared_ptr_t root;
+    Compare cmp_fun;
+
+    template<typename Callable>
+    void _in_order(shared_ptr_t current, const Callable& fun) {
+        // Si el arbol es vacio
+        if (!current) return;
+        // Ir al arbol izquierdo
+        _in_order(current->left, fun);
+        // Obtener el valor
+        fun(current->item);
+        // Ir al arbor derecho
+        _in_order(current->right, fun);
+    }
 
 public:
     binary_search_tree() = default;
 
+    void insert(const item_t& item) {
+        if (!root) { // si no existe
+            root = make_shared<node_t>(item, nullptr);
+            return;
+        }
+        auto current = root;
+        while(current) { // mientras existe
+            if (cmp_fun(item.first, current->item.first)) {
+                if (!current->left) {
+                    current->left = make_shared<node_t>(item, current);
+                    break;
+                }
+                else {
+                    current = current->left;
+                }
+            }
+            else {
+                if (!current->right) {
+                    current->right = make_shared<node_t>(item, current);
+                    break;
+                }
+                else {
+                    current = current->right;
+                }
+            }
+        }
+    }
+
+    template<typename Callable>
+    void bfs(const Callable& fun) {
+        // Crear el queue
+        queue<shared_ptr_t> q;
+        // Realizar el push del root
+        q.push(root);
+        // Mientras el queue no este vacio
+        while (!q.empty()) {
+            // Obtener el front
+            auto current = q.front();
+            fun(current->item);
+            // Extraer el front (pop)
+            q.pop();
+            // Agregar los hijos del nodo extraido
+            if (current->left) q.push(current->left);
+            if (current->right) q.push(current->right);
+        }
+    }
+
+    template<typename Callable>
+    void in_order(const Callable& fun) {
+        _in_order(root, fun);
+    }
 };
 
 #endif //PROG3_TEO3_2023_ARBOLES_BINARY_SEARCH_TREE_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "binary_search_tree.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    binary_search_tree<int, string> bst;
+    bst.insert({40, "A"});
+    bst.insert({10, "B"});
+    bst.insert({50, "C"});
+    bst.insert({60, "D"});
+    bst.insert({30, "E"});
+    bst.insert({20, "F"});
+    bst.bfs([](const auto& item){
+        cout << item.first << " " << item.second << endl;
+    });
+    cout << "---------------\n";
+    bst.in_order([](const auto& item){
+        cout << item.first << " " << item.second << endl;
+    });
     return 0;
 }
